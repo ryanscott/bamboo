bb5ba07b0a3939ac1dd0eb7984e8ead2dfcef43e
Ryan Stubblefield <ryan.stubblefield@gmail.com>
2010-05-09 14:21:29 -0700

    support for the iPad simulator, so I don't have to switch back and forth between 3.2 / 3.1.3 to build / run

--- samples/friendgraph/iPad/AppDelegate_Pad.h
+++ samples/friendgraph/iPad/AppDelegate_Pad.h
@@ -1,9 +1,13 @@
+@class MainController;
+
 @interface AppDelegate_Pad : NSObject <UIApplicationDelegate> 
 {
 	UIWindow *window;
+	MainController* _mainController;
 }
 
 @property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) MainController* _mainController;
 
 @end
 
--- samples/friendgraph/iPad/AppDelegate_Pad.m
+++ samples/friendgraph/iPad/AppDelegate_Pad.m
@@ -1,13 +1,21 @@
 #import "AppDelegate_Pad.h"
+#import "MainController.h"
 
 @implementation AppDelegate_Pad
 
 @synthesize window;
+@synthesize _mainController;
 
 -(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions 
 {
+	initRCLib();
+	_mainController = [[MainController alloc] init];
+	
 	window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
 	window.backgroundColor = [UIColor greenColor];
+	
+	[window addSubview:self._mainController.view];
+
 	[window makeKeyAndVisible];
 	
 	return YES;
@@ -15,6 +23,8 @@
 
 -(void)dealloc 
 {
+	terminateRCLib();
+	[_mainController release];
 	[window release];
 	[super dealloc];
 }
